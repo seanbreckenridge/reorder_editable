c45af0d98ca3ac9bd2e43bbafc14ca5487d7358f
Sean Breckenridge <seanbrecke@gmail.com>
2023-09-25 14:35:14 -0700

    move static setup config to setup.cfg

--- reorder_editable/__init__.py
+++ reorder_editable/__init__.py
@@ -1,1 +1,3 @@
 from .core import Editable, ReorderEditableError
+
+__all__ = ["Editable", "ReorderEditableError"]
--- setup.py
+++ setup.py
@@ -1,41 +1,4 @@
-from pathlib import Path
 from setuptools import setup
 
-long_description = Path("README.md").read_text()
-reqs = Path("requirements.txt").read_text().strip().splitlines()
-
-pkg = "reorder_editable"
-setup(
-    name=pkg,
-    version="0.1.0",
-    url="https://github.com/seanbreckenridge/reorder_editable",
-    author="Sean Breckenridge",
-    author_email="seanbrecke@gmail.com",
-    description=("""naive implementation to reorder my easy-install.pth file"""),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    packages=["reorder_editable"],
-    install_requires=reqs,
-    package_data={pkg: ["py.typed"]},
-    zip_safe=False,
-    keywords="",
-    entry_points={
-        "console_scripts": ["reorder_editable = reorder_editable.__main__:main"]
-    },
-    extras_require={
-        "testing": [
-            "pytest",
-            "mypy",
-        ]
-    },
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-)
+if __name__ == "__main__":
+    setup()
